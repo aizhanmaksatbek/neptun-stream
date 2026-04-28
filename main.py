3fccdf573e70ded4ee4ce6ca69caa989b8ddbba0
Aizhan <aizhan1maks@gmail.com>
2026-04-28 17:49:02 +0800

    Added SQLmodel database and table creation on startup. Added functionality of database interaction to get and post articles.

--- main.py
+++ main.py
@@ -1,22 +1,59 @@
-from fastapi import FastAPI
-import random
+from fastapi import FastAPI, Depends, HTTPException, Query
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+from typing import Annotated
 
 
+class Article(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(index=True)
+    content: str = Field(index=True)
+
+
+sqlite_file_name = "database_neptun.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
 app = FastAPI()
 
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
 @app.get("/articles/")
-def get_home_page() -> dict:
+def get_home_page(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100
+) -> list[Article]:
     """This function returns the article ids and titles.
 
     Returns:
-        dict: home page content
+        list[Article]: list of articles
     """
-    return {random.randint(1, 1000): "title"}
+    articles = session.exec(select(Article).offset(offset).limit(limit)).all()
+    return articles
 
 
 @app.get("/articles/{article_id}")
-def get_article(article_id: int) -> dict:
+def get_article(
+    article_id: int,
+    session: SessionDep
+) -> Article:
     """
     This function retrieves the article title and content by its id.
 
@@ -24,18 +61,23 @@ def get_article(article_id: int) -> dict:
         article_id (int): id of the article
 
     Returns:
-        dict: article title, content, id
+        Article: article title, content, id
     """
 
-    return {
-        "title": "title",
-        "content": "content",
-        "article_id": article_id
-        }
+    article = session.get(Article, article_id)
+    if not article:
+        raise HTTPException(
+            status_code=404,
+            detail="Article {article_id} not found"
+            )
+    return article
 
 
 @app.post("/articles/")
-def publish_article(title: str, content: str) -> int:
+def publish_article(
+    article: Article,
+    session: SessionDep
+) -> Article:
     """This function saves the article in database using unique id.
     Parameters:
         title (str): title of the article
@@ -44,4 +86,7 @@ def publish_article(title: str, content: str) -> int:
         int: unique id of the article from the stored database
     """
 
-    return random.randint(1, 1000)
+    session.add(article)
+    session.commit()
+    session.refresh(article)
+    return article
