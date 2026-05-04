1aebc1268483d47e35147b26d03b6bbce7a05506
Aizhan <aizhan1maks@gmail.com>
2026-05-04 14:55:26 +0800

    Make engine creation global.

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,8 @@
 from ..config.settings import sqlite_url, connect_args
 from sqlmodel import create_engine, Session
 
+engine = create_engine(sqlite_url, connect_args=connect_args)
 
 def get_session():
-    engine = create_engine(sqlite_url, connect_args=connect_args)
     with Session(engine) as session:
         yield session
