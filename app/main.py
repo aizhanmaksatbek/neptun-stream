944a517469fa867a333fc6951c3c082c23bb061b
Aizhan <aizhan1maks@gmail.com>
2026-05-11 15:54:45 +0800

    Changed the on_event("startup") event handler with asynccontextmanager with lifespan app parameter

--- app/main.py
+++ app/main.py
@@ -3,19 +3,20 @@ from .config.settings import sqlite_url, connect_args
 from .alembic.scripts import create_db_and_tables
 from sqlmodel import create_engine
 from fastapi import FastAPI
-
+from contextlib import asynccontextmanager
 import uvicorn
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    engine = create_engine(sqlite_url, connect_args=connect_args)
+    create_db_and_tables(engine)
+    yield
+    engine.clear()
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(articles.router)
 app.include_router(users.router)
 
-
-@app.on_event("startup")
-def on_startup():
-    engine = create_engine(sqlite_url, connect_args=connect_args)
-    create_db_and_tables(engine)
 
 
 if __name__ == "__main__":
