530a2b2dcceaef54bc3cf703f480e128848792fc
Aizhan <aizhan1maks@gmail.com>
2026-06-06 16:46:24 +0800

    Added home html page using Jinja2 template.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from .routers import articles, users
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+import requests
 
 app = FastAPI()
 app.include_router(articles.router)
 app.include_router(users.router)
+
+templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/", include_in_schema=False)
+def home(request: Request):
+    articles = requests.get("http://localhost:8000/articles/").json()
+    return templates.TemplateResponse(
+        request, "home.html", {"articles": articles}
+        )
