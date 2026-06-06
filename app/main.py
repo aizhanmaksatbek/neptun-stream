cad5d72b4c7c0d5638bf866d883206ef26749d24
Aizhan <aizhan1maks@gmail.com>
2026-06-06 18:07:33 +0800

    Updated layout, home html files with css styles and formating.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 from .routers import articles, users
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import requests
 
 app = FastAPI()
 app.include_router(articles.router)
 app.include_router(users.router)
 
-templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory="app/frontend/templates")
+app.mount(
+    "/static",
+    StaticFiles(directory="app/frontend/static"),
+    name="static"
+    )
 
 
 @app.get("/", include_in_schema=False)
